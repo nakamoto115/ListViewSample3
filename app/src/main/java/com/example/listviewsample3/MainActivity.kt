2dfc79ba99529229d9801c65889767b676c4b16d
nakamoto115 <129806964+nakamoto115@users.noreply.github.com>
2023-10-19 14:35:07 +0900

    帰宅

--- app/src/main/java/com/example/listviewsample3/MainActivity.kt
+++ app/src/main/java/com/example/listviewsample3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.listviewsample3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+
+        var menuList = mutableListOf("牛丼定食", "からあげ定食", "カレー定食", "焼き魚定食", "唐揚げ定食", "天ぷら定食", "焼肉定食", "とんかつ定食", "鰻定食", "寿司定食", "天丼定食", "豆腐定食", "焼き鳥定食", "麺類定食", "刺身定食")
+
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+
+        lvMenu.adapter = adapter
     }
 }
