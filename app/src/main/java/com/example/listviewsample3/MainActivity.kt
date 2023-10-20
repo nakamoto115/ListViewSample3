5024b5a53f2599e498d9b2686177d68512d108bd
nakamoto115 <129806964+nakamoto115@users.noreply.github.com>
2023-10-20 14:36:42 +0900

    帰宅

--- app/src/main/java/com/example/listviewsample3/MainActivity.kt
+++ app/src/main/java/com/example/listviewsample3/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.listviewsample3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -11,11 +13,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.onItemClickListener = ListItemClickListener()
 
         var menuList = mutableListOf("牛丼定食", "からあげ定食", "カレー定食", "焼き魚定食", "唐揚げ定食", "天ぷら定食", "焼肉定食", "とんかつ定食", "鰻定食", "寿司定食", "天丼定食", "豆腐定食", "焼き鳥定食", "麺類定食", "刺身定食")
 
         val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
 
         lvMenu.adapter = adapter
+    }
+
+    public inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
     }
 }
